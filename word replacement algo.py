201503fd135a4d1a81fb20c3f2a3c2ad8c3e8d6b
Quarshie Emmanuel <skyemmanuel42@gmail.com>
2024-07-15 16:43:27 +0000

    Create word replacement algo.py

---
+++ word replacement algo.py
@@ -0,0 +1,6 @@
+print("WORD REPLACEMENT PROGRAM")
+sentence= input("Enter your sentence")
+print("Your sentence is " +sentence)
+word1= input('enter the word to replace: ' )
+word2= input ("enter the word to replace it with: ")
+print(sentence.replace(word1,word2))
