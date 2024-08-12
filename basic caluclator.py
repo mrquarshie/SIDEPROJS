72bd966925bdd4de74b773aef8cf30b4e1579740
codeANUEL <52331668+quarshie233@users.noreply.github.com>
2024-08-12 08:18:27 +0000

    Add files via upload

---
+++ basic caluclator.py
@@ -0,0 +1,12 @@
+#basic caluclator
+num1=int(input("enter first number "))
+num2=int(input("enter second number "))
+op=(input("Enter your operator "))
+if op=="+":
+    print("the result is ",num1+num2)
+if op=="-":
+    print("the result is ", num1-num2)
+if op=="*":
+    print("the product if values is ", num1*num2)
+if op=="/":
+    print("the result is ",num1/num2)
